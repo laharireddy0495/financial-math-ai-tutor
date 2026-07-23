ba4ab122e48236cc47bf02de1e71bc7b951ecaff
LaharshithaReddy-CSE <laharshithareddykolli@gmail.com>
2026-07-24 01:24:23 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -42,7 +42,7 @@ Student:
     try:
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.5-flash-lite"
             contents=prompt,
         )
 
